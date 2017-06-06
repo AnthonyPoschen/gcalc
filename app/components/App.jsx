c4c12cad97c5a72517f7954fa210d51b232da9c5
Anthony "Zanven" Poschen <zanven42@gmail.com>
2017-06-07 05:44:38 +1000

    React + Mobx integrated
    also got a default hello world app with react + mobx. 
    
    todo: make compiled final output program.

---
+++ app/components/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+const remote = require('electron').remote;
+
+@observer
+export default class App extends React.Component {
+  render() {
+    const { height, width } = remote.getCurrentWindow().getBounds();
+    return (
+      <div style={{ height: height - 25, width: width, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+        <marquee><pre>Hello poop!</pre></marquee>
+        <marquee><pre>    Hello poop!</pre></marquee>
+        <marquee><pre>        Hello poop!</pre></marquee>
+        <marquee><pre>            Hello poop!</pre></marquee>
+        <marquee><pre>                Hello poop!</pre></marquee>
+        <marquee><pre>                    Hello poop!</pre></marquee>
+        <marquee><pre>                        Hello poop!</pre></marquee>
+        <marquee><pre>                            Hello poop!</pre></marquee>
+        <marquee><pre>                                Hello poop!</pre></marquee>
+        <marquee><pre>                                    Hello poop!</pre></marquee>
+        <marquee><pre>                                        Hello poop!</pre></marquee>
+        <marquee><pre>                                            Hello poop!</pre></marquee>
+        <marquee><pre>                                                Hello poop!</pre></marquee>
+        <marquee><pre>                                                    Hello poop!</pre></marquee>
+        <marquee><pre>                                                        Hello poop!</pre></marquee>
+        <marquee><pre>                                                            Hello poop!</pre></marquee>
+        <marquee><pre>                                                                Hello poop!</pre></marquee>
+        <marquee><pre>                                                                    Hello poop!</pre></marquee>
+        <marquee><pre>                                                                        Hello poop!</pre></marquee>
+        <marquee><pre>                                                                            Hello poop!</pre></marquee>
+      </div>
+    );
+  }
+}
